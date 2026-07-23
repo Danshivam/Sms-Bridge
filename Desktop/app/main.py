2d0202c9660127a529799a72564a4d66f80c4a52
Shivam <xkrish3@gmail.com>
2026-07-23 10:10:18 +0530

    Implement Android to FastAPI communication

--- Desktop/app/main.py
+++ Desktop/app/main.py
@@ -1,13 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(
     title="SMS Bridge",
     version="1.0.0"
 )
 
+
+class TestMessage(BaseModel):
+    message: str
+
+
 @app.get("/")
 def home():
     return {
         "status": "online",
         "message": "SMS Bridge Server Running"
+    }
+
+
+@app.post("/message")
+def receive_message(message: TestMessage):
+
+    print("\n==============================")
+    print("Received from Android")
+    print(message.message)
+    print("==============================\n")
+
+    return {
+        "status": "received"
     }
